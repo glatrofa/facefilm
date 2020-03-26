71349d7b721c3601c4a3c1a74300d51a2085e21e
Vito Difonzo <vitodifonzo1998@gmail.com>
2020-03-26 18:49:34 +0100

    vito

--- src/php/db-connection.php
+++ src/php/db-connection.php
@@ -4,7 +4,7 @@ $dbPassword = "password";
 $dbUserName = "nome_utente";
 $dbServer = "localhost";
 $dbName = "awwa";
-//CIAO PEPPE
+//CIAO PEPPE CIAO
 $connection = new mysqli($dbServer, $dbUserName, $dbPassword, $dbName);
 if($connection->connect_errno){
     exit("Database Connection Failed. Reason: ".$connection->connect_error);
