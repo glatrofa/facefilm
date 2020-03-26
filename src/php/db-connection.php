ec6b06c93708c4736d53d7db114e5b277402a047
Vito Difonzo <vitodifonzo1998@gmail.com>
2020-03-26 18:42:32 +0100

    vito

--- src/php/db-connection.php
+++ src/php/db-connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$dbPassword = "";
-$dbUserName = "";
-$dbServer = "";
-$dbName = "";
+$dbPassword = "password";
+$dbUserName = "nome_utente";
+$dbServer = "localhost";
+$dbName = "awwa";
 
 $connection = new mysqli($dbServer, $dbUserName, $dbPassword, $dbName);
 if($connection->connect_errno){
