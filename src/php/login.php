8ab76bbc9c14af45c268ab8d3e86e9e6738dd8ce
Vito Difonzo <vitodifonzo1998@gmail.com>
2020-03-26 18:57:50 +0100

    modifica

--- src/php/login.php
+++ src/php/login.php
@@ -2,20 +2,20 @@
 
 include 'db-info.php';
 
-$username = mysqli_real_escape_string($connection, $_POST["username"]);
+$username = mysqli_real_escape_string($connection, $_POST["nome_utente"]);
 $password = mysqli_real_escape_string($connection, $_POST["password"]);
 $password_criptata = md5($password);
 
-$query = "SELECT username, email FROM user WHERE username = '".$username."' AND password = '".$password_criptata."'";
+$query = "SELECT nome_utente, email FROM utenti WHERE nome_utente = '".$username."' AND password = '".$password_criptata."'";
 $result = mysqli_query($connection, $query) or die("Access failed");
 $rowsNumber = mysqli_num_rows($result);
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 $response = array();
 if($rowsNumber != 0){
-    $response[0] = array('username' => $row["username"], 'email' => $row["email"]);  
+    $response[0] = array('nome_utente' => $row["nome_utente"], 'email' => $row["email"]);  
 }
 else{
-    $response[0] = array('username' => null, 'email' => null);
+    $response[0] = array('nome_utente' => null, 'email' => null);
 }
 $json_data = json_encode($response);
 echo $json_data;
