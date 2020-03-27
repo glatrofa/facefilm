9b1871cecbe4a129ac7817c1ccf97e2b9bff65eb
Vito Difonzo <vitodifonzo1998@gmail.com>
2020-03-27 18:35:39 +0100

    right

--- src/php/registrazione.php
+++ src/php/registrazione.php
@@ -40,11 +40,11 @@ mysql_query("SELECT * FROM $utenti WHERE email = '$email'");
 $nome = mysqli_real_escape_string($connection, $_POST["nome"]);
 $cognome = mysqli_real_escape_string($connection, $_POST["cognome"]);
 $data_nascita = mysqli_real_escape_string($connection, $_POST["data_nascita"]);
-$nazione = mysqli_real_escape_string($connection, $_POST["nazione"]);
+//$nazione = mysqli_real_escape_string($connection, $_POST["nazione"]);
 $email = mysqli_real_escape_string($connection, $_POST["email"]);
 $username = mysqli_real_escape_string($connection, $_POST["username"]);
 $password = mysqli_real_escape_string($connection, $_POST["password"]);
-$query=("INSERT INTO `utenti` (`nome`, `cognome`, `data_nascita`, `nazione`, `email`, `nome_utente`, `password`) VALUES ($nome, $cognome , $data_nascita, $nazione, $email, $username, $password);");
+$query=("INSERT INTO `utenti` (`nome`, `cognome`, `data_nascita`, `nazione`, `email`, `nome_utente`, `password`) VALUES ($nome, $cognome , $data_nascita, 'italia', $email, $username, $password);");
 //$query=("INSERT INTO `utenti` (`nome`, `cognome`, `data_nascita`, `nazione`, `email`, `nome_utente`, `password`) VALUES ($nome, $cognome , '', '', 'cluado@fragolina.it', 'clod', 'cludio.fragolA');");
 $result = mysqli_query($connection, $query) or die("Access failed");
 ?>
