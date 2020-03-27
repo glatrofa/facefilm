441e0e1bce2a33752b3504fe096f6925ebcc57d8
Vito Difonzo <vitodifonzo1998@gmail.com>
2020-03-27 16:59:54 +0100

    v1.0

--- src/php/registrazione.php
+++ src/php/registrazione.php
@@ -11,3 +11,18 @@ $username = mysqli_real_escape_string($connection, $_POST["username"]);
 $password = mysqli_real_escape_string($connection, $_POST["password"]);
 
 mysql_query("INSERT INTO ".$tbl_name." (nome,cognome,data_nascita,nazione,email,username,password) VALUES ('$nome','$cognome','$data_nascita','$nazione','$email','$username','$password')",$db)
+$result = mysqli_query($connection, $query) or die("Access failed");
+$rowsNumber = mysqli_num_rows($result);
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+$response = array();
+if($rowsNumber != 0){ 
+    echo "Registrazione effettuata";
+}
+else{
+    $response[0] = array('nome_utente' => null, 'email' => null);
+    echo "Registrazione non effettuata";
+}
+$json_data = json_encode($response);
+echo $json_data;
+
+?>
