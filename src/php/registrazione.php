2305837f132fceb130bb0665126b29142498846a
Vito Difonzo <vitodifonzo1998@gmail.com>
2020-03-27 18:41:12 +0100

    v3

--- src/php/registrazione.php
+++ src/php/registrazione.php
@@ -11,6 +11,6 @@ $username = mysqli_real_escape_string($connection, $_POST["nome_utente"]);
 $password = mysqli_real_escape_string($connection, $_POST["password"]);
 
 $query=("INSERT INTO `utenti` (`nome`, `cognome`, `data_nascita`, `nazione`, `email`, `nome_utente`, `password`) VALUES ($nome, $cognome , $data_nascita, 'italia', $email, $username, $password);");
-//$query=("INSERT INTO `utenti` (`nome`, `cognome`, `data_nascita`, `nazione`, `email`, `nome_utente`, `password`) VALUES ($nome, $cognome , '', '', 'cluado@fragolina.it', 'clod', 'cludio.fragolA');");
+
 $result = mysqli_query($connection, $query) or die("Access failed");
 ?>
