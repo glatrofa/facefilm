3d998e8a0427d9b9b2be64a1c3e7a00467fd8f3d
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-05-02 19:42:23 +0200

    Modificata query con email e aggiunta password con md5

--- src/php/login.php
+++ src/php/login.php
@@ -1,23 +1,22 @@
 <?php
 
+//header('Access-Control-Allow-Origin: *');
 include 'db-connection.php';
 
-$username = mysqli_real_escape_string($connection, $_POST["username"]);
+$email = mysqli_real_escape_string($connection, $_POST["email"]);
 $password = mysqli_real_escape_string($connection, $_POST["password"]);
+$passwordCriptata = md5($password);
 
-
-$query = "SELECT nome_utente, email FROM utenti WHERE nome_utente = '".$username."' AND password = '".$password."'";
+$query = "SELECT nome_utente, email FROM utenti WHERE email = '".$email."' AND password = '".$passwordCriptata."'";
 $result = mysqli_query($connection, $query) or die("Access failed");
 $rowsNumber = mysqli_num_rows($result);
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 $response = array();
 if($rowsNumber != 0){
     $response[0] = array('nome_utente' => $row["nome_utente"], 'email' => $row["email"]);  
-    echo "Login effettuato";
 }
 else{
     $response[0] = array('nome_utente' => null, 'email' => null);
-    echo "Utente non presente";
 }
 $json_data = json_encode($response);
 echo $json_data;
