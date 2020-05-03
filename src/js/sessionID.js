d6069de11cdadc13196e9f35eb5c3efab723ff23
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-05-03 16:50:28 +0200

    Eliminato log suprefluo

--- src/js/sessionID.js
+++ src/js/sessionID.js
@@ -1,9 +1,7 @@
 // controlla id parametro con l'id del cookie PHPSESSID
 function checkID(id) {
-    if(document.cookie.split(';').some((item) => item.includes('PHPSESSID='+id+''))){
+    if(document.cookie.split(';').some((item) => item.includes('PHPSESSID='+id+'')))
         return true;
-        console.log('gli id corrispondono');
-    }
     else{
         window.location.href = 'http://awwa.sytes.net/html/login.html';
         return false;
