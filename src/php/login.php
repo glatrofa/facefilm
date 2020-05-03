753a542c7ca3ddf8031f0e3b2068da09d43942d0
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-05-03 10:16:56 +0200

    Fix invio di valori vuoti in post

--- src/php/login.php
+++ src/php/login.php
@@ -1,31 +1,22 @@
 <?php
 
-//header('Access-Control-Allow-Origin: *');
 include 'db-connection.php';
 
 $email = mysqli_real_escape_string($connection, $_POST["email"]);
 $password = mysqli_real_escape_string($connection, $_POST["password"]);
 $passwordCriptata = md5($password);
-/*
+
 $query = "SELECT nome_utente, email FROM utenti WHERE email = '".$email."' AND password = '".$passwordCriptata."'";
 $result = mysqli_query($connection, $query) or die("Access failed");
 $rowsNumber = mysqli_num_rows($result);
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-$response = array();
 if($rowsNumber != 0){
-    $response[0] = array('nome_utente' => $row["nome_utente"], 'email' => $row["email"]);  
-    $response[1] = 'login';
+    $response = array('nome_utente' => $row["nome_utente"], 'email' => $row["email"]);  
 }
 else{
-    $response[0] = array('nome_utente' => null, 'email' => null);
-    $response[1] = 'NO login';
+    $response = array('nome_utente' => null, 'email' => null);
 }
-*/
-//$response = array('email' => $email, 'password' => $password, 'pc' => $passwordCriptata);  
-$response = array();
-$response[0] = $email;
-$response[1] = $password;
-$response[2] = $passwordCriptata;
+
 $jsonData = json_encode($response);
 echo $jsonData;
 
