8c6c11206f81ccbcc618b619838847fe53fdd72a
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-05-03 16:11:24 +0200

    Aggiunto commento

--- src/php/connessioneDatabase.php
+++ src/php/connessioneDatabase.php
@@ -1,9 +1,16 @@
 <?php
 
+
 $dbPassword = "Dtr1m0n%ci";
 $dbUserName = "giuseppe";
 $dbServer = "localhost";
 $dbName = "awwa";
+/*
+$dbPassword = "";
+$dbUserName = "root";
+$dbServer = "localhost";
+$dbName = "awwa";
+*/
 
 $connection = new mysqli($dbServer, $dbUserName, $dbPassword, $dbName);
 if($connection->connect_errno){
