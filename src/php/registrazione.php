e943e8e42af37f366639138a186880adfd519097
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-05-03 17:51:57 +0200

    Fix query

--- src/php/registrazione.php
+++ src/php/registrazione.php
@@ -12,7 +12,7 @@ $password = mysqli_real_escape_string($connection, $_POST["password"]);
 $password_criptata = md5($password);
 
 $query=("INSERT INTO 'utenti' ('nome', 'cognome', 'data_nascita', 'nazione', 'email', 'nome_utente', 'password')"
-        ."VALUES ('".$nome."', '".$cognome."' , '".$data_nascita."', '".$nazione."', '".$email."', '".$username."', '".$password_criptata."');");
+        ."VALUES ('".$nome."', '".$cognome."' , '".$data_nascita."', '".$nazione."', '".$email."', '".$username."', '".$password_criptata."')");
 $result = mysqli_query($connection, $query) or die("Access failed");
 
 ?>
