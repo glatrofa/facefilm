dacb8c42c3477160d29e3b426ca712f9aa637790
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-05-03 11:40:03 +0200

    Aggiunto uso sessione

--- src/php/login.php
+++ src/php/login.php
@@ -2,6 +2,8 @@
 
 include 'db-connection.php';
 
+session_start();
+
 $email = mysqli_real_escape_string($connection, $_POST["email"]);
 $password = mysqli_real_escape_string($connection, $_POST["password"]);
 $passwordCriptata = md5($password);
@@ -13,6 +15,7 @@ $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 $response = array();
 if($rowsNumber != 0){
     $response[0] = array('nome_utente' => $row["nome_utente"], 'email' => $row["email"]);  
+    $_SESSION[$row["nome_utente"]] = true;
 }
 else{
     $queryEmailPresente = "SELECT email FROM utenti WHERE email = '".$email."'";
@@ -23,6 +26,7 @@ else{
     else{
         $response[0] = array('email' => null);
     }
+    session_destroy();
 }
 
 $jsonData = json_encode($response);
