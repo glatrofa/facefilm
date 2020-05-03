90beafce30bc8a24f799b2737b0151d917a64aca
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-05-03 15:33:48 +0200

    Modificato path cookie

--- src/php/autenticazione.php
+++ src/php/autenticazione.php
@@ -8,7 +8,7 @@ function creaToken() {
 function creaCookie($email) {
     $token = creaToken();
     // crea un cookie della durata di 1 giorno (86400 * 30) o 1 ora (3600)
-    setcookie($email, $token, time() + 3600, "../");
+    setcookie($email, $token, time() + 3600, "/");
     return $token;
 }
 
