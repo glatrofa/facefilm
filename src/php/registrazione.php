ddc67ad8cf6b278c41a5474734f0229159f6d1b9
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-05-03 17:44:58 +0200

    Fix registrazione

--- src/php/registrazione.php
+++ src/php/registrazione.php
@@ -5,12 +5,14 @@ include './connessioneDatabase.php';
 $nome = mysqli_real_escape_string($connection, $_POST["nome"]);
 $cognome = mysqli_real_escape_string($connection, $_POST["cognome"]);
 $data_nascita = mysqli_real_escape_string($connection, $_POST["data_nascita"]);
-//$nazione = mysqli_real_escape_string($connection, $_POST["nazione"]);
+$nazione = mysqli_real_escape_string($connection, $_POST["nazione"]);
 $email = mysqli_real_escape_string($connection, $_POST["email"]);
 $username = mysqli_real_escape_string($connection, $_POST["nome_utente"]);
 $password = mysqli_real_escape_string($connection, $_POST["password"]);
+$password_criptata = md5($password);
 
-$query=("INSERT INTO `utenti` (`nome`, `cognome`, `data_nascita`, `nazione`, `email`, `nome_utente`, `password`) VALUES ('".$nome."', '".$cognome."' , '".$data_nascita."', 'italia', '".$email."', '".$username."', '".$password."');");
-
+$query=("INSERT INTO 'utenti' ('nome', 'cognome', 'data_nascita', 'nazione', 'email', 'nome_utente', 'password')"
+        ."VALUES ('".$nome."', '".$cognome."' , '".$data_nascita."', '".$nazione."', '".$email."', '".$username."', '".$password_criptata."');");
 $result = mysqli_query($connection, $query) or die("Access failed");
+
 ?>
