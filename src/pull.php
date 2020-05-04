87332a6094ddb80bb7755d9978f85150ebc074c3
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-05-04 16:54:37 +0200

    Modificato script di esecuzione

--- src/pull.php
+++ src/pull.php
@@ -1,7 +1,7 @@
 <?php
 
 if ( $_POST['payload'] ) {
-    shell_exec( ‘cd /var/www/drupalsites/git-repo/ && git reset –hard HEAD && git pull’ );
+    shell_exec( ‘cd ~ && ./refresh-site’ );
 }
 
 ?>hi
