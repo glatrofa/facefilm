c1796c66f815059b51224db2dbb9e78373f17ae0
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-05-04 18:51:22 +0200

    aggiunto echo risultato comandi

--- src/php/pull.php
+++ src/php/pull.php
@@ -2,8 +2,9 @@
 
 if ($_SERVER['HTTP_X_GITHUB_EVENT'] == 'push') {
     echo "ciao";
-    shell_exec("cd ~" && "./refresh-site");
-    //shell_exec("./refresh-site");    
+    echo shell_exec("cd ~");
+    echo shell_exec("./refresh-site");    
+    echo "fine script";
 }
 
 ?>
