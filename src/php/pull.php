9f6e6db4d5d60a8b9aba8c242f23cc020040f930
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-05-04 19:02:08 +0200

    Eliminato righe inutili

--- src/php/pull.php
+++ src/php/pull.php
@@ -3,12 +3,8 @@
 $path = "/var/www/facefilm";
 
 if ($_SERVER['HTTP_X_GITHUB_EVENT'] == 'push') {
-    echo "ciao";
     echo shell_exec("cd {$path}");
     echo shell_exec("sudo -u www-data git pull");
-    //echo shell_exec("./refresh-site");    
-    //echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
-    echo "fine script";
 }
 
 ?>
