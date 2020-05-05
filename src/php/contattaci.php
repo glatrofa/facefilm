7f096dc9ce2e69030f278c514deede9180589336
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-05-05 10:56:21 +0200

    modificato file per l'inserimento dei dati nel db

--- src/php/contattaci.php
+++ src/php/contattaci.php
@@ -1,4 +1,24 @@
 <?php
+
+include './connessioneDatabase.php';
+
+$nome = mysqli_real_escape_string($connection, $_POST["nome"]);
+$messaggio = mysqli_real_escape_string($connection, $_POST["messaggio"]);
+$oggetto = mysqli_real_escape_string($connection, $_POST["oggetto"]);
+$email = mysqli_real_escape_string($connection, $_POST["email"]);
+
+$queryL = "LOCK TABLES messaggi WRITE";
+mysqli_query($connection, $queryL) or die(mysqli_error($connection));
+$query = "INSERT INTO messaggi (nome, email, oggetto, messaggio)"
+    ."VALUES ('$nome', '$email' , '$oggetto', '$messaggio')";
+$result = mysqli_query($connection, $query) or die(mysqli_error($connection));
+$queryU = "UNLOCK TABLES";
+mysqli_query($connection, $queryU) or die (mysqli_error($connection));    
+
+$json_data = json_encode($result);
+echo $json_data; 
+
+/*
 if(isset( $_POST['name']))
 $name = $_POST['name'];
 if(isset( $_POST['email']))
@@ -13,4 +33,6 @@ $recipient = "g.latrofa3@studenti.poliba.it";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $content, $mailheader) or die(mysqli_error($connection));
 echo "Email sent!";
+*/
+
 ?>
