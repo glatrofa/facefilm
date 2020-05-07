01b6647f4a8088ae607053f0f07c8451d404fbf7
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-05-07 18:53:40 +0200

    aggiunto selezione di serie, stagione ed episodio per la pubblicazione del post

--- src/js/home.js
+++ src/js/home.js
@@ -2,7 +2,7 @@ import { Logged } from './autenticazione.js';
 
 // verifica che l'utente abbia effettuato l'accesso
 //window.onpaint = logged();
-window.onload = Logged();
+//window.onload = Logged();
 
 const colorPrimary = '#e5af05';
 const colorSecondary = '#00008b';
@@ -80,3 +80,90 @@ function VisualizzaClassifica() {
         })
         .catch(err => { throw err });
 }
+
+// ricerca nel db tutte le serie comprendenti nel nome i caratteri inseriti nel form
+$(function VisualizzaSerie() {
+    $('#cerca_serie').keyup(function CercaNomeSerie() {
+        let api = 'd278f4116f977c4c40da51f004832a5a';
+        let url = 'https://api.themoviedb.org/3/search/tv?api_key='+api+'&language=it&page=1&query='+$(this).val()+'&include_adult=true';
+        fetch(url)
+            .then(res => res.json())
+            .then((data) => {
+                console.log('Checkout this JSON! ', data);
+                //console.log('dimensione '+data.results.length);
+                let i = 0;            
+                let listaSerie = '<option value="null"> Seleziona </option>';
+                // ritornano al massimo 20 risultato per volta
+                while (data.results.length >= 1 && i < data.results.length) {
+                    listaSerie += '<option value='+data.results[i].id+'>'+data.results[i].name+'</option>';
+                    i ++;
+                }      
+                if (data.results.length == 0)
+                    listaSerie = '<option value='+'null'+'> --- </option>';
+                document.getElementById('post_serie').innerHTML = listaSerie;
+            })
+            .catch(err => { throw err });
+        //let idSerie = $('#post_serie :selected').val();
+        //VisualizzaStagioni(api, idSerie);
+        //$('#post_serie').change(VisualizzaStagioni());
+    });
+});
+
+$(function VisualizzaStagioni() {
+    $('#post_serie').change( function() {        
+        let api = 'd278f4116f977c4c40da51f004832a5a';
+        let idSerie = $('#post_serie :selected').val();
+        let url = 'https://api.themoviedb.org/3/tv/'+idSerie+'?api_key='+api+'&language=it';
+        fetch(url)
+            .then(res => res.json())
+            .then((data) => {
+                console.log('result', data);
+                console.log('numero stagioni ', data.number_of_seasons);
+                console.log('stagioni ', data.seasons);
+                let listaStagioni = '<option value="null"> Seleziona </option>';
+                if(data.number_of_seasons != 0) {
+                    let j = 0;
+                    //if(data.seasons[0] == null)
+                        //j = 1;                          
+                    while (j < data.number_of_seasons) {
+                        listaStagioni += '<option value='+data.seasons[j].season_number+'>'+data.seasons[j].name+'</option>';
+                        j ++;
+                    }      
+                    if (data.length == 0)
+                        listaStagioni = '<option value='+'null'+'> --- </option>';                
+                }
+                else
+                    listaStagioni = '<option value='+'null'+'>Nessuna stagione</option>';
+                document.getElementById('post_stagione').innerHTML = listaStagioni;
+            })
+            .catch(err => { throw err });
+    });
+});
+
+$(function VisualizzaEpisodi() {
+    $('#post_stagione').change( function() {
+        let api = 'd278f4116f977c4c40da51f004832a5a';
+        let idSerie = $('#post_serie :selected').val();
+        let numeroStagione = $('#post_stagione :selected').val();
+        let url = 'https://api.themoviedb.org/3/tv/'+idSerie+'/season/'+numeroStagione+'?api_key='+api+'&language=it';
+        fetch(url)
+            .then(res => res.json())
+            .then((data) => {
+                console.log('episodi', data);
+                let listaEpisodi = '<option value="null"> Seleziona </option>';           
+                if(data.episodes.length != 0){
+                    let i = 0;                 
+                    while (i < data.episodes.length) {
+                        listaEpisodi += '<option value='+data.episodes[i].episode_number+'>'+data.episodes[i].name+'</option>';
+                        i ++;
+                    }      
+                    if (data.length == 0)
+                        listaEpisodi = '<option value='+'null'+'> --- </option>';
+                }
+                else
+                    listaEpisodi = '<option value='+'null'+'>Nessun episodio</option>';
+                document.getElementById('post_episodio').innerHTML = listaEpisodi;
+            })
+            .catch(err => { throw err });
+    });    
+});
