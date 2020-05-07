132c15aa4c520b1a87ba06452e4b2a34df2e862a
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-05-07 15:26:31 +0200

    fix url e cambio nome oggetto json restituito dall'api

--- src/js/cerca_serie_tv.js
+++ src/js/cerca_serie_tv.js
@@ -6,7 +6,7 @@ import { Logged } from './autenticazione.js';
 // effettua il redirect sulla pagina della serie selezionata
 $('#cerca_serie_bottone').click(function CercaSerie() {
     //console.log('serie selezionata '+$('#mostra_nome_serie :selected').val());
-    window.location.href = './cerca_serie_tv.html?id='+$('#mostra_nome_serie :selected').val()+'';
+    window.location.href = './serie_tv.html?id='+$('#mostra_nome_serie :selected').val()+'';
 });
 
 // ricerca nel db tutte le serie comprendenti nel nome i caratteri inseriti nel form
@@ -15,17 +15,17 @@ $('#cerca_serie').keyup(function CercaNomeSerie() {
     let url = 'https://api.themoviedb.org/3/search/tv?api_key='+api+'&language=it&page=1&query='+$(this).val()+'&include_adult=true';
     fetch(url)
         .then(res => res.json())
-        .then((out) => {
-            console.log('Checkout this JSON! ', out);
-            console.log('dimensione '+out.results.length);
+        .then((data) => {
+            console.log('Checkout this JSON! ', data);
+            console.log('dimensione '+data.results.length);
             let i = 0;            
             let lista = '';
             // ritornano al massimo 20 risultato per volta
-            while (out.results.length >= 1 && i < out.results.length) {
-                lista += '<option value='+out.results[i].id+'>'+out.results[i].name+'</option>';
+            while (data.results.length >= 1 && i < data.results.length) {
+                lista += '<option value='+data.results[i].id+'>'+data.results[i].name+'</option>';
                 i ++;
             }      
-            if (out.results.length == 0)
+            if (data.results.length == 0)
                 lista = '<option value='+'null'+'> --- </option>';
             document.getElementById('mostra_nome_serie').innerHTML = lista;
         })
