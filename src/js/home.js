aefa19e9e4ecaa7c31e1b2ffec50e86197b1ba36
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-05-08 18:24:02 +0200

    fix nome variabile

--- src/js/home.js
+++ src/js/home.js
@@ -180,9 +180,9 @@ $(function login() {
         dataType: 'json',
         success: function (data) {            
             console.log('SUCCESS '+data);
-            let data = Date.parse(data[2]);
+            let dataPubblicazione = Date.parse(data[2]);
             //console.log('data '+data[2].getDate()+'/'+data[2].getMonth()+'/'+data[2].getFullYear()+' ora '+data[2].getHours+':'+data[2].getMinutes()+':'+data[2].getSeconds());
-            console.log(' data '+data);
+            console.log(' data '+dataPubblicazione);
         },
         error: function (data) {
           console.log('ERROR '+data);
