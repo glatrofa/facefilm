e68243c8f224d606e92bbfbeeeba8516846ea9b6
Alessandro Difonzo <alexdif980@gmail.com>
2020-05-08 20:26:36 +0200

    .

--- src/js/cerca_serie_tv.js
+++ src/js/cerca_serie_tv.js
@@ -1,10 +1,7 @@
 import { logged } from './autenticazione.js';
 import {APIKEY} from './key.js';
-import {getBaseImageURL} from './indirizzo_base_img.js';
-console.log(getBaseImageURL());
-
 // verifica che l'utente abbia effettuato l'accesso
-window.onload = logged();
+// window.onload = logged();
 
 // richiama funzioni non appena il documento è caricato
 $(document).ready(function() {
@@ -43,25 +40,31 @@ $('#cerca_serie').keyup(function cercaNomeSerie() {
 // in una serie di card, DA FINIRE
 $('#mostra_poster').click(function cercaPoster() {
     let url2 = 'https://api.themoviedb.org/3/search/tv?api_key=' + APIKEY + '&language=it&page=1&query=' + $('#cerca_serie').val()
-    //console.log(url2);
+    console.log(url2);
     fetch(url2)
     .then(res => res.json())
     .then((data) => {
-        let cards = '';
-        if (data.results.lenght) {
+        console.log('Checkout this JSON! ', data);
+        document.getElementById('risultati_ricerca').innerHTML = '';
+        const baseImageURL = 'https://image.tmdb.org/t/p/';
+        console.log(baseImageURL);
+        if (data.results.length) {
             //per ogni elemento dell'array dei risultati crea una card con poster e descrizione serie, DA FINIRE
-            data.results.forEach(element => {
-                card = '<div class="card shadow">' +
-                                '<img class="card-img-top" src="' + getBaseImageURL().concat('w') + '" alt="Poster">' +
+            for (var element of data.results) {
+                let cards =  '<div class="card col-5 col-lg-3 px-0 mx-3 mb-3 shadow"' +
+                                '<img class="card-img-top" src="' + baseImageURL.concat('w342', element.poster_path) + '" alt="Poster">' +
                                 '<div class="card-body">' +
-                                    '<div class="card-title">Titolo serie</div>' +
-                                    '<p>qui ci potrebbe stare una breve descrizione della serie, magari quella fornita da API ma in versione ridotta</p>' +
+                                    '<div class="card-title font-weight-bold">' + element.name + '</div>' +
+                                    '<p class="text-truncate h7">' + element.overview + '</p>' +
                                 '</div>' +
-                                '<div class="card-footer"><span class="text-muted">Data pubblicazione</span></div>'+
+                                '<div class="card-footer"><span class="text-muted">' + element.first_air_date.substr(0,4) + '</span></div>'+
+                            '</div>' +
                         '</div>';
-            });
+                document.getElementById('risultati_ricerca').innerHTML += cards;
+            }
         }
     })
+    .catch((err) => {console.log(err)});
     
 });
 
