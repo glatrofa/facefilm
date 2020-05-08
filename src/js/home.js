b183ecb0a4d9e4d18e663dea67abce70ce8f0ef5
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-05-08 13:52:49 +0200

    fix modal

--- src/js/home.js
+++ src/js/home.js
@@ -2,7 +2,6 @@ import { logged } from './autenticazione.js';
 import {APIKEY} from './key.js';
 
 // verifica che l'utente abbia effettuato l'accesso
-//window.onpaint = logged();
 //window.onload = logged();
 
 const colorPrimary = '#e5af05';
