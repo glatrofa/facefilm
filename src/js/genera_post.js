e24491dfe1e4b85f860624058b863a0bbecd25bc
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-05-09 14:42:22 +0200

    aggiunta rappresentazione data

--- src/js/genera_post.js
+++ src/js/genera_post.js
@@ -32,10 +32,10 @@ function generaHeader(nomeUtente, idPost) {
 function generaBody(data, titolo, testo, idSerie, stagione, episodio) {
     //console.log('tipo data ', typeof(data));
     console.log('vecchia data ', data);
-    //data = data.substring(0, data.indexOf('.'));
-    //console.log('data fomatatta', data);
+    data = data.substring(0, data.indexOf('.'));
+    console.log('data fomatatta', data);
     data = new Date(data * 1000);
-    console.log('nuova data ', data.get);
+    console.log('nuova data ', data.getDay() + '/' + data.getMonth() + '/' + data.getFullYear() + ' - ' + data.getHours() + ':' + data.getMinutes() + ':' + data.getSeconda());
     let body = '<div class="card-body">' +
                     '<div class="text-muted h7 mb-2"> <i class="fa fa-clock-o"></i> '+ data +'</div>' +
                     '<a class="card-link">' +
