854b0362e7388cc498b35008e3eb5cdc1ebc9e7b
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-05-09 14:45:02 +0200

    fix dichiarazione dataFormattat

--- src/js/genera_post.js
+++ src/js/genera_post.js
@@ -36,7 +36,7 @@ function generaBody(data, titolo, testo, idSerie, stagione, episodio) {
     console.log('data fomatatta', data);
     data = new Date(data * 1000);
     //console.log('nuova data ', data.getDay() + '/' + data.getMonth() + '/' + data.getFullYear() + ' - ' + data.getHours() + ':' + data.getMinutes() + ':' + data.getSeconds());
-    dataFormattata = data.getDay() + '/' + data.getMonth() + '/' + data.getFullYear() + ' - ' + data.getHours() + ':' + data.getMinutes() + ':' + data.getSeconds();
+    let dataFormattata = data.getDay() + '/' + data.getMonth() + '/' + data.getFullYear() + ' - ' + data.getHours() + ':' + data.getMinutes() + ':' + data.getSeconds();
     let body = '<div class="card-body">' +
                     '<div class="text-muted h7 mb-2"> <i class="fa fa-clock-o"></i> '+ dataFormattata +'</div>' +
                     '<a class="card-link">' +
