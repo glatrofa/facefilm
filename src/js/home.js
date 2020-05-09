528019255ed5c550ecf115e058ccfe4021b7af7a
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-05-09 18:16:40 +0200

    fix

--- src/js/home.js
+++ src/js/home.js
@@ -253,21 +253,21 @@ function redirectHome() {
 */
 
 // mostra nella home i post più recenti
-function visualizzaPost(){
-    let data = ottieniDatiPost();
+function visualizzaPost(post){
+    //let data = ottieniDatiPost();
     //console.log('SUCCESS '+data[0]+);
     //console.log('post 1', data[0]);
     //console.log('post 2', data[1]);
-    let i;
-    document.getElementById("sezione_post").innerHTML = "";
+    //let i;
+    document.getElementById("sezione_post").innerHTML = "" + post;
     // visualizza a schermo tutti i post
-    for (i = 1; i <= data[0]; i++) {
-        console.log('post '+ i +'', data[i]);
-        document.getElementById("sezione_post").innerHTML += generaHeader(data[i].nomeUtente, data[i].idPost)+generaBody(data[i].data, data[i].titolo, data[i].testo, data[i].idSerie, data[i].stagione, data[i].episodio)+generaFooter(data[i].idPost, data[i].idSerie, data[i].like, data[i].dislike, data[i].numeroCommenti);
+    //for (i = 1; i <= data[0]; i++) {
+        //console.log('post '+ i +'', data[i]);
+        //document.getElementById("sezione_post").innerHTML += generaHeader(data[i].nomeUtente, data[i].idPost)+generaBody(data[i].data, data[i].titolo, data[i].testo, data[i].idSerie, data[i].stagione, data[i].episodio)+generaFooter(data[i].idPost, data[i].idSerie, data[i].like, data[i].dislike, data[i].numeroCommenti);
         //document.getElementById("sezione_post").innerHTML += generaFooter(data[i].idPost, data[i].idSerie, data[i].like, data[i].dislike, data[i].numeroCommenti);
         //let footer = "<span class='badge badge-pill bg-awwa-primary mr-2'>12</span><a class='card-link awwa-primary post' id='190' name='post_like' data-toggle='modal' href=''><i class='fa fa-diamond'></i> Mi piace </a>";                                                       
         //document.getElementById("sezione_post").innerHTML = footer;
-    }
+    //}
 }
 
 function ottieniDatiPost() {
@@ -278,6 +278,7 @@ function ottieniDatiPost() {
         dataType: 'json',
         success: function (data) {
             //console.log('SUCCESS ' + data);
+            /*
             let post = [];
             post[0] = data[0];
             let i;
@@ -286,6 +287,13 @@ function ottieniDatiPost() {
             }
             console.log('SUCCESS ' + post);
             return post;
+            */
+            let post = "";
+            let i;
+            for (i = 0; i < data.length; i++) {    
+                post += generaHeader(data[i].nomeUtente, data[i].idPost)+generaBody(data[i].data, data[i].titolo, data[i].testo, data[i].idSerie, data[i].stagione, data[i].episodio)+generaFooter(data[i].idPost, data[i].idSerie, data[i].like, data[i].dislike, data[i].numeroCommenti);                
+            }
+
         },
         error: function (data) {
             console.log('ERROR ' + data);
