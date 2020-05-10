79ed6d70c816c37c32f44c6b754fb267f6e06bae
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-05-10 15:13:34 +0200

    file creato per il reperimento dei commenti relativi ad un post

---
+++ src/php/ottieni_commenti.php
@@ -0,0 +1,29 @@
+<?php
+
+include './connessioneDatabase.php';
+
+// definizione variabili dal post
+$idPost = mysqli_real_escape_string($connection, $_POST["idPost"]);
+
+// esecuzione query
+$query = "SELECT commenti.id, commenti.data, commenti.testo, utenti.nome_utente FROM commenti INNER JOIN utenti ON commenti.id_utente = utenti.email WHERE id_post = '".$idPost."'";
+$response = array();
+$result = mysqli_query($connection, $query) or die($response[0] = mysqli_error($connection));
+$rowsNumber = mysqli_num_rows($result);
+if($rowsNumber != 0){
+    // ritorno dei dati
+    $i = 0;
+    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+        $response[$i] = array('idCommento' => $row["id"], 'data' => $row["data"], 'testo' => $row["testo"], 'nomeUtente' => $row["nome_utente"]);
+        $i ++;
+    }    
+}
+else{
+    $response[0] = null;
+}
+
+// ritorno dati al client
+$jsonData = json_encode($response);
+echo $jsonData;
+
+?>
