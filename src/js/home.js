3976b158413b80c2bd23deaa4c8652cd8edefbda
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-05-10 16:01:41 +0200

    fix visualizzazione commenti

--- src/js/home.js
+++ src/js/home.js
@@ -352,14 +352,13 @@ function visualizzaPost() {
                         dataType: 'json',
                         success: function (data) {
                             console.log("successo ajax " + data[0].testo + ' ' + data[0].nomeUtente + ' ' + data[0].data);
-                            if(data[0] != null){
+                            document.getElementById("modal_commenti_container").innerHTML = "";
+                            let i;
+                            for (i = 0; i < data.length; i++) {    
+                                document.getElementById("modal_commenti_container").innerHTML += visualizzaCommenti(data[i].data, data[i].testo, data[i].nomeUtente);
+                            }
+                            if(data[0] == null){
                                 //document.getElementById("modal_commenti_container").innerHTML = visualizzaModalCommento(idPost);
-                                document.getElementById("modal_commenti_container").innerHTML = "";
-                                let j;
-                                for (j = 0; j < data.length; j++) {    
-                                    document.getElementById("modal_commenti_container").innerHTML += visualizzaCommenti(data[j].data, data[j].testo, data[j].nomeUtente);
-                                }
-                            } else {
                                 document.getElementById("modal_commenti_container").innerHTML = "Nessun commento";
                             }
                             $('#modal_commenti').modal('show');
