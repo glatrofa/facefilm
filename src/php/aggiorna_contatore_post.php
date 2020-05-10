71aa0bd1b658b070286a6fd16d8922fa52c78f05
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-05-10 10:00:41 +0200

    risposta resa vettore per debug

--- src/php/aggiorna_contatore_post.php
+++ src/php/aggiorna_contatore_post.php
@@ -7,38 +7,39 @@ include './connessioneDatabase.php';
 //$idPost = mysqli_real_escape_string($connection, $_POST["idPost"]);
 $obiettivo = "piace";
 $idPost = 7;
-$respone = true;
+$respone = array();
+$respone[0] = true;
 
 if($obiettivo == "piace") {
     // lock tabelle
     $queryL = "LOCK TABLES post WRITE";
-    mysqli_query($connection, $queryL) or die($response = mysqli_error($connection));
+    mysqli_query($connection, $queryL) or die($response[1] = mysqli_error($connection));
     // prelievo vecchio valore
     $query = "SELECT piace FROM post WHERE id = '".$idPost."'";
-    $result = mysqli_query($connection, $query) or die($response = mysqli_error($connection));
+    $result = mysqli_query($connection, $query) or die($response[2] = mysqli_error($connection));
     $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
     $row = $row["piace"] ++;
     // inserimento nuovo valore
     $query = "UPDATE post SET piace = ".$row." WHERE id = '".$idPost."'";
-    mysqli_query($connection, $query) or die($response = mysqli_error($connection));
+    mysqli_query($connection, $query) or die($response[3] = mysqli_error($connection));
     // unlock tabelle
     $queryU = "UNLOCK TABLES";
-    mysqli_query($connection, $queryU) or die ($response = mysqli_error($connection));
+    mysqli_query($connection, $queryU) or die ($response[4] = mysqli_error($connection));
 } else if($obiettivo == "dislike") {
     // lock tabelle
     $queryL = "LOCK TABLES post WRITE";
-    mysqli_query($connection, $queryL) or die($response = mysqli_error($connection));
+    mysqli_query($connection, $queryL) or die($response[5] = mysqli_error($connection));
     // prelievo vecchio valore
     $query = "SELECT dislike FROM post WHERE id = '".$idPost."'";
-    $result = mysqli_query($connection, $query) or die($response = mysqli_error($connection));
+    $result = mysqli_query($connection, $query) or die($response[6] = mysqli_error($connection));
     $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
     $row = $row["dislike"] ++;
     // inserimento nuovo valore
     $query = "UPDATE post SET dislike = ".$row." WHERE id = '".$idPost."'";
-    mysqli_query($connection, $query) or die($response = mysqli_error($connection));
+    mysqli_query($connection, $query) or die($response[7] = mysqli_error($connection));
     // unlock tabelle
     $queryU = "UNLOCK TABLES";
-    mysqli_query($connection, $queryU) or die ($response = mysqli_error($connection));
+    mysqli_query($connection, $queryU) or die ($response[8] = mysqli_error($connection));
 } else {
     $respone = "nessun obiettivo selezionato";
 }
