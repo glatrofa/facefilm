175e4dcd9810d54729fcc2b2efb23e382925ea95
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-05-10 18:24:52 +0200

    db aggiornato con i commenti

--- src/database/awwa.sql
+++ src/database/awwa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Creato il: Mag 09, 2020 alle 11:24
+-- Creato il: Mag 10, 2020 alle 17:24
 -- Versione del server: 10.3.22-MariaDB-0+deb10u1
 -- Versione PHP: 7.3.14-1~deb10u1
 
@@ -40,7 +40,10 @@ CREATE TABLE `commenti` (
 
 INSERT INTO `commenti` (`id`, `id_utente`, `id_post`, `testo`, `data`) VALUES
 (1, 'g.latrofa3@studenti.poliba.it', 4, 'bello', '2020-05-09 10:17:13.00000'),
-(2, 'g.latrofa3@studenti.poliba.it', 5, 'non mi piace', '2020-05-09 10:21:36.00000');
+(2, 'g.latrofa3@studenti.poliba.it', 5, 'non mi piace', '2020-05-09 10:21:36.00000'),
+(10, 'g.latrofa3@studenti.poliba.it', 8, 'vito scemo', '2020-05-10 15:23:15.31062'),
+(9, 'g.latrofa3@studenti.poliba.it', 9, 'vito scemo', '2020-05-10 15:23:15.30364'),
+(8, 'g.latrofa3@studenti.poliba.it', 9, 'test', '2020-05-10 15:22:01.39729');
 
 -- --------------------------------------------------------
 
@@ -81,8 +84,12 @@ CREATE TABLE `post` (
 --
 
 INSERT INTO `post` (`id`, `email`, `data`, `titolo`, `testo`, `id_serie`, `numero_stagione`, `numero_episodio`, `piace`, `dislike`) VALUES
-(5, 'g.latrofa3@studenti.poliba.it', '2020-05-08 17:36:52.39811', 'titolo', 'testo', '18347', '5', '13', 0, 0),
-(4, 'g.latrofa3@studenti.poliba.it', '2020-05-08 17:34:20.78757', 'falena titolo', 'bla bla bla', '4607', '1', '7', 0, 0);
+(5, 'g.latrofa3@studenti.poliba.it', '2020-05-08 17:36:52.39811', 'titolo', 'testo', '18347', '5', '13', 7, 9),
+(4, 'g.latrofa3@studenti.poliba.it', '2020-05-08 17:34:20.78757', 'falena titolo', 'bla bla bla', '4607', '1', '7', 12, 6),
+(6, 'g.latrofa3@studenti.poliba.it', '2020-05-09 12:45:59.87789', 'covid', 'covid 19', '75758', '1', '6', 13, 6),
+(7, 'g.latrofa3@studenti.poliba.it', '2020-05-09 12:49:00.09076', 'ale toglie sempre il login', 'alessandro non deve togliere il login', '63054', '1', '2', 16, 17),
+(8, 'g.latrofa3@studenti.poliba.it', '2020-05-10 09:45:33.60735', 'Stagione 4', 'Non vedo l\'ora che esca la quarta stagione!', '60625', '3', '10', 2, 1),
+(9, 'g.latrofa3@studenti.poliba.it', '2020-05-10 09:52:27.15041', 'Wow che serie', 'Davvero una bellissima serie, peccato che si debba cerca una spiegazione del finale. È davvero criptico!', '4607', '5', '17', 6, 1);
 
 -- --------------------------------------------------------
 
@@ -96,6 +103,24 @@ CREATE TABLE `segnalazioni` (
   `email` varchar(50) NOT NULL,
   `data` timestamp(5) NOT NULL DEFAULT current_timestamp(5)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dump dei dati per la tabella `segnalazioni`
+--
+
+INSERT INTO `segnalazioni` (`id`, `idPost`, `email`, `data`) VALUES
+(1, 7, '', '2020-05-10 09:17:53.67588'),
+(2, 6, '', '2020-05-10 09:18:06.20523'),
+(3, 7, '', '2020-05-10 09:20:42.41639'),
+(4, 4, '', '2020-05-10 09:22:04.51246'),
+(5, 6, '', '2020-05-10 09:22:10.94631'),
+(6, 7, '', '2020-05-10 09:22:14.14685'),
+(7, 7, '', '2020-05-10 09:23:30.84431'),
+(8, 7, '', '2020-05-10 09:35:10.82342'),
+(9, 4, '', '2020-05-10 09:35:13.70879'),
+(10, 5, '', '2020-05-10 09:35:15.71571'),
+(11, 5, '', '2020-05-10 09:35:18.46122'),
+(12, 7, '', '2020-05-10 09:44:50.29973');
 
 -- --------------------------------------------------------
 
@@ -169,7 +194,7 @@ ALTER TABLE `utenti`
 -- AUTO_INCREMENT per la tabella `commenti`
 --
 ALTER TABLE `commenti`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT per la tabella `messaggi`
 --
@@ -179,12 +204,12 @@ ALTER TABLE `messaggi`
 -- AUTO_INCREMENT per la tabella `post`
 --
 ALTER TABLE `post`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT per la tabella `segnalazioni`
 --
 ALTER TABLE `segnalazioni`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
