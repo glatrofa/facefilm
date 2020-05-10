608eaf7b9a77c5f9ded13c0921c0fdec3a9c7fcb
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-05-10 18:06:52 +0200

    aa

--- src/php/pull.php
+++ src/php/pull.php
@@ -2,11 +2,11 @@
 // non funziona l'avvio dello script sul server
 $path = "/var/www/facefilm";
 
-if ($_SERVER['HTTP_X_GITHUB_EVENT'] == 'push') {
+//if ($_SERVER['HTTP_X_GITHUB_EVENT'] == 'push') {
     //echo shell_exec("cd {$path}");
     //echo shell_exec("sudo -u www-data git pull");
     echo shell_exec("./rs");
     echo "fine script";
-}
+//}
 
 ?>
