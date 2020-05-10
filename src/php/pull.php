93da33b89c05a2ebdefd18ebc5d4c9df377c3c87
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-05-10 18:04:35 +0200

    echo shell exec

--- src/php/pull.php
+++ src/php/pull.php
@@ -5,7 +5,7 @@ $path = "/var/www/facefilm";
 if ($_SERVER['HTTP_X_GITHUB_EVENT'] == 'push') {
     //echo shell_exec("cd {$path}");
     //echo shell_exec("sudo -u www-data git pull");
-    shell_exec("./rs");
+    echo shell_exec("./rs");
     echo "fine script";
 }
 
