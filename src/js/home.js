428afc84c2c9ce91460a294f573b0a5cd2ca9f6c
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-05-14 15:21:17 +0200

    fix lettura post terminati

--- src/js/home.js
+++ src/js/home.js
@@ -255,7 +255,7 @@ function visualizzaPost(pagina) {
             //document.getElementById("sezione_post").innerHTML = "";
             let i;
             for (i = 0; i < data.length; i++) {
-                if(typeof data[i] != 'undefined')
+                if(typeof data[i].nomeUtente != 'undefined')
                     document.getElementById("sezione_post").innerHTML += generaHeader(data[i].nomeUtente, data[i].idPost)+generaBody(data[i].data, data[i].titolo, data[i].testo, data[i].idSerie, data[i].stagione, data[i].episodio)+generaFooter(data[i].idPost, data[i].idSerie, data[i].like, data[i].dislike, data[i].numeroCommenti);                
                 else
                     alert("post terminati");
