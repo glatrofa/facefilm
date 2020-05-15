e3b486106c866254c4efce404716da55d04c832b
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-05-15 16:13:16 +0200

    fix debug in caso di fallimento registrazione

--- src/js/registrazione.js
+++ src/js/registrazione.js
@@ -26,7 +26,7 @@ $(function registrazione() {
           }          
         },
         error: function (data) {
-          console.log(data[0]);
+          console.log(data);
           $('#modal-registrazione-error').modal('show');
         }
       });
