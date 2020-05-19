b368cfb592b173a460c4c19799fdaf97a09704d6
Alessandro Difonzo <alexdif980@gmail.com>
2020-05-19 13:30:53 +0200

    .

--- src/js/home.js
+++ src/js/home.js
@@ -476,7 +476,7 @@ $(function logout() {
             crossOrigin: true,
             success: function () {
                 document.cookie = "PHPSESSID=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-                location.href = '.';
+                location.href = './html/login.html';
             }
         });
     });
