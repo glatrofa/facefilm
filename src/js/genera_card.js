7bf2943376150c717cc8459c7ee94a2b5f63fe2a
Alessandro Difonzo <alexdif980@gmail.com>
2020-06-03 01:12:49 +0200

    .

--- src/js/genera_card.js
+++ src/js/genera_card.js
@@ -1,9 +1,12 @@
 
 function generaCard(element, baseImageURL) {
     let card =  '<div class="card col-5 col-lg-3 px-0 mx-3 mb-3 shadow" id="' + element.id +'">' +
-                        '<a data-toggle="tooltip" data-placement="top" title="Vai alla pagina della serie" href="./serie_tv.html?id=' + element.id + '"><img class="card-img-top" src="';
+                        '<a data-toggle="tooltip" data-placement="top" title="Vai alla pagina della serie" href="./serie_tv.html?id=' + element.id + '">' + 
+                            '<img class="card-img-top" src="';
     if (element.poster_path) {card += baseImageURL.concat('w342', element.poster_path)}
-    card +=                 '" alt="Poster"></a>' +
+    else {card += '../img/eye-slash-solid.svg'}
+    card +=                 '" alt="Poster">' +
+                        '</a>' +
                         '<div class="card-body text-center">' +
                             '<div class="card-title font-weight-bold">' + element.name + '</div>' +
                             '<button onclick="window.location.href = \'./serie_tv.html?id=' + element.id + '\';" class="btn">Vai alla pagina</button>' +
