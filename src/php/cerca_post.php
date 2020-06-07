5fa41a3d3adfd3005299f588af5493cc86c90eed
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-06-07 15:19:05 +0200

    formattato query e rimosso query escaped

--- src/php/cerca_post.php
+++ src/php/cerca_post.php
@@ -22,12 +22,27 @@ if($idSerie != null && $numeroStagione != null && $numeroEpisodio != null)
              "ORDER BY data DESC ".
              "LIMIT ".$pagina.", ".$postPerPagina."";
 else if($idSerie != null && $numeroStagione != null)
-    $query = "SELECT idPost, numeroCommenti, UNIX_TIMESTAMP(data) AS data, titolo, testo, id_serie, numero_stagione, numero_episodio, piace, dislike, utenti.nome_utente, immagine FROM utenti INNER JOIN (SELECT post.id AS idPost, COUNT(commenti.id) AS numeroCommenti, post.data, post.titolo, post.testo, post.id_serie, post.numero_stagione, post.numero_episodio, post.email, post.piace, post.dislike FROM post LEFT JOIN commenti ON post.id = commenti.id_post WHERE post.id_serie = ".$idSerie." AND post.numero_stagione = ".$numeroStagione." GROUP BY idPost) AS x ON x.email = utenti.email ORDER BY data DESC LIMIT ".$pagina.", ".$postPerPagina."";
+    $query = "SELECT idPost, numeroCommenti, UNIX_TIMESTAMP(data) AS data, titolo, testo, id_serie, numero_stagione, numero_episodio, piace, dislike, utenti.nome_utente, immagine ".
+             "FROM utenti INNER JOIN (SELECT post.id AS idPost, COUNT(commenti.id) AS numeroCommenti, post.data, post.titolo, post.testo, post.id_serie, post.numero_stagione, post.numero_episodio, post.email, post.piace, post.dislike ".
+                "FROM post LEFT JOIN commenti ".
+                "ON post.id = commenti.id_post ".
+                "WHERE post.id_serie = ".$idSerie." AND post.numero_stagione = ".$numeroStagione.
+                " GROUP BY idPost) AS x ".
+             "ON x.email = utenti.email ".
+             "ORDER BY data DESC ".
+             "LIMIT ".$pagina.", ".$postPerPagina."";
 if($idSerie != null)
-    $query = "SELECT idPost, numeroCommenti, UNIX_TIMESTAMP(data) AS data, titolo, testo, id_serie, numero_stagione, numero_episodio, piace, dislike, utenti.nome_utente, immagine FROM utenti INNER JOIN (SELECT post.id AS idPost, COUNT(commenti.id) AS numeroCommenti, post.data, post.titolo, post.testo, post.id_serie, post.numero_stagione, post.numero_episodio, post.email, post.piace, post.dislike FROM post LEFT JOIN commenti ON post.id = commenti.id_post WHERE post.id_serie = ".$idSerie." GROUP BY idPost) AS x ON x.email = utenti.email ORDER BY data DESC LIMIT ".$pagina.", ".$postPerPagina."";
-$queryEscaped = mysqli_real_escape_string($connection, $query);
+    $query = "SELECT idPost, numeroCommenti, UNIX_TIMESTAMP(data) AS data, titolo, testo, id_serie, numero_stagione, numero_episodio, piace, dislike, utenti.nome_utente, immagine ".
+             "FROM utenti INNER JOIN (SELECT post.id AS idPost, COUNT(commenti.id) AS numeroCommenti, post.data, post.titolo, post.testo, post.id_serie, post.numero_stagione, post.numero_episodio, post.email, post.piace, post.dislike ".
+                "FROM post LEFT JOIN commenti ON post.id = commenti.id_post ".
+                "WHERE post.id_serie = ".$idSerie.
+                " GROUP BY idPost) AS x ".
+             "ON x.email = utenti.email ".
+             "ORDER BY data DESC ".
+             "LIMIT ".$pagina.", ".$postPerPagina."";
+//$queryEscaped = mysqli_real_escape_string($connection, $query);
 $response = array();
-$result = mysqli_query($connection, $queryEscaped) or die($response[0] = mysqli_error($connection));
+$result = mysqli_query($connection, $query) or die($response[0] = mysqli_error($connection));
 $rowsNumber = mysqli_num_rows($result);
 if($rowsNumber != 0){
     // ritorno dei dati
