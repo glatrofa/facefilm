e6a9b60be1688a8c92fd0b3269fa9136a80c6551
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-06-07 12:56:35 +0200

    aggiunto versione w3c

--- src/php/image.php
+++ src/php/image.php
@@ -1,50 +1,49 @@
 <?php
-/*include("config.php");
+$target_dir = "../img/users_image/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-if(isset($_POST['but_upload'])){
- 
-  $name = $_FILES['file']['name'];
-  $target_dir = "/img/users_image/";
-  $target_file = $target_dir . basename($_FILES["file"]["name"]);
-
-  // Select file type
-  $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
-  // Valid file extensions
-  $extensions_arr = array("jpg","jpeg","png","gif");
-
-  // Check extension
-  if( in_array($imageFileType,$extensions_arr) ){
-  
-     // Upload file
-     move_uploaded_file($_FILES['file']['tmp_name'],$target_dir.$name);
-
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
   }
- 
-  $image_src = "/img/users_image/Carlino con tastiera e mouse.png";
 }
-*/
-$uploaddir = '/img/users_image/';
-$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
 
-echo "<p>";
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
+}
 
-if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-  echo "File is valid, and was successfully uploaded.\n";
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
 } else {
-   echo "Upload failed";
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
 }
-
-echo "</p>";
-echo '<pre>';
-echo 'Here is some more debugging info:';
-print_r($_FILES);
-print "</pre>";
-phpinfo();
 ?>
-
-<form enctype="multipart/form-data" action="upload.php" method="POST">
-    <input type="hidden" name="MAX_FILE_SIZE" value="512000" />
-    Send this file: <input name="userfile" type="file" />
-    <input type="submit" value="Send File" />
-</form>
