2e96bf70f466f46b8369771d29aabb39c6cf957b
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-06-07 09:23:38 +0200

    aggiunto recupero informazioni utente e commentato il recupero dei post

--- src/php/profilo.php
+++ src/php/profilo.php
@@ -2,30 +2,47 @@
 
 include './connessioneDatabase.php';
 // recupero identificativo dell'utente loggato
-$email_utente = mysqli_real_escape_string($connection, $_SESSION["email"]);
+$email = mysqli_real_escape_string($connection, $_SESSION["email"]);
 // recupero pagina
-$pagina = mysqli_real_escape_string($connection, $_POST["pagina"]);
+//$pagina = mysqli_real_escape_string($connection, $_POST["pagina"]);
 // imposto il numero di post da visualizzare volta per volta
-$postPerPagina = 2;
-
-// esecuzione query
-$query =   "SELECT idPost, numeroCommenti, UNIX_TIMESTAMP(data) AS data, titolo, testo, id_serie, numero_stagione, numero_episodio, piace, dislike, utenti.nome_utente, immagine 
-            FROM utenti INNER JOIN (SELECT post.id AS idPost, COUNT(commenti.id) AS numeroCommenti, post.data, post.titolo, post.testo, post.id_serie, post.numero_stagione, post.numero_episodio, post.email, post.piace, post.dislike 
-                                    FROM post LEFT JOIN commenti ON post.id = commenti.id_post 
-                                    GROUP BY idPost) AS x ON x.email = utenti.email
-            WHERE email = " . $email_utente . "
-            ORDER BY data DESC 
-            LIMIT " . $pagina . ", " . $postPerPagina . "";
-
-$query_escaped = mysqli_real_escape_string($connection, $query);
-
+//$postPerPagina = 2;
+/*
+// definizione query recupero post dell'utente
+$queryPost = "SELECT idPost, numeroCommenti, UNIX_TIMESTAMP(data) AS data, titolo, testo, id_serie, numero_stagione, numero_episodio, piace, dislike, utenti.nome_utente, immagine ". 
+            "FROM utenti INNER JOIN (SELECT post.id AS idPost, COUNT(commenti.id) AS numeroCommenti, post.data, post.titolo, post.testo, post.id_serie, post.numero_stagione, post.numero_episodio, post.email, post.piace, post.dislike ".
+                                    "FROM post LEFT JOIN commenti ON post.id = commenti.id_post ".
+                                    "GROUP BY idPost) AS x ON x.email = utenti.email ".
+            "WHERE email = " . $email . " ".
+            "ORDER BY data DESC ".
+            "LIMIT " . $pagina . ", " . $postPerPagina;
+            */
+// definizione query recupero informazioni utente        
+$queryUtente = "SELECT nome, cognome, data_nascita, nazione, nome_utente, immagine ".
+                "FROM utenti ".
+                "WHERE email = ". $email;
+// definizione query sicure
+$queryPostEscaped = mysqli_real_escape_string($connection, $queryPost);
+//$queryUtenteEscaped = mysqli_real_escape_string($connection, $queryUtente);
 $response = array();
-$result = mysqli_query($connection, $query_escaped) or die($response[0] = mysqli_error($connection));
+// esecuzione query utente
+$resultUtente = mysqli_query($connection, $queryUtenteEscaped) or die($response[0] = mysqli_error($connection));
+$rowUtente = mysqli_fetch_array($result, MYSQLI_ASSOC);
+$response[0] = array(   'nomeUtente' => $row["nome_utente"],
+                        'nome' => $row["nome"],
+                        'cognome' => $row["cognome"],
+                        'data_nascita' => $row["data_nascita"],
+                        'nazione' => $row["nazione"],
+                        'immagine' => $row["immagine"]
+                    );
+                    /*
+// esecuzione query post                    
+$result = mysqli_query($connection, $queryPostEscaped) or die($response[0] = mysqli_error($connection));
 $rowsNumber = mysqli_num_rows($result);
 
 if($rowsNumber != 0){
     // ritorno dei dati
-    $i = 0;
+    $i = 1;
     while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
         $response[$i] = array(  'nomeUtente' => $row["nome_utente"], 
                                 'idPost' => $row["idPost"], 
@@ -41,11 +58,12 @@ if($rowsNumber != 0){
                                 'immagine' => $row["immagine"]
                             );
         $i ++;
-    }    
+    }       
 }
 else{
-    $response[1] = null;
+    $response[0] = null;
 }
+*/
 
 // ritorno dati al client
 $jsonData = json_encode($response);
