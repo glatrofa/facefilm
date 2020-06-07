d3fc7415f1f219f1eafff3991a98e8b8c0ac3a36
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-06-07 14:59:59 +0200

    rimossa query escaped

--- src/php/profilo.php
+++ src/php/profilo.php
@@ -8,9 +8,8 @@ session_start();
 $email = mysqli_real_escape_string($connection, $_SESSION["email"]);
 // definizione query recupero informazioni utente        
 $query = "SELECT nome_utente, nome, cognome, data_nascita, nazione, immagine FROM utenti WHERE email = '". $email ."'";
-$queryEscaped = mysqli_real_escape_string($connection, $query);
 // esecuzione query
-$result = mysqli_query($connection, $queryEscaped) or die($response = mysqli_error($connection));
+$result = mysqli_query($connection, $query) or die($response = mysqli_error($connection));
 // ritorno dei dati
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 $response = array(  'nomeUtente' => $row["nome_utente"],
