dacc80974ffbd497f89b129c482eb957946e28cf
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-06-07 11:58:53 +0200

    aggiunto invio mail

--- src/php/contattaci.php
+++ src/php/contattaci.php
@@ -2,12 +2,13 @@
 
 include './connessione_database.php';
 
-// definizione variabili dal post
+// prelievo variabili dal client
 $nome = mysqli_real_escape_string($connection, $_POST["nome"]);
 $messaggio = mysqli_real_escape_string($connection, $_POST["messaggio"]);
 $oggetto = mysqli_real_escape_string($connection, $_POST["oggetto"]);
 $email = mysqli_real_escape_string($connection, $_POST["email"]);
 
+/*
 // lock tabella
 $queryL = "LOCK TABLES messaggi WRITE";
 mysqli_query($connection, $queryL) or die(mysqli_error($connection));
@@ -22,22 +23,13 @@ mysqli_query($connection, $queryU) or die (mysqli_error($connection));
 // ritorno dati al client
 $json_data = json_encode($result);
 echo $json_data; 
-
-/*
-if(isset( $_POST['name']))
-$name = $_POST['name'];
-if(isset( $_POST['email']))
-$email = $_POST['email'];
-if(isset( $_POST['message']))
-$message = $_POST['message'];
-if(isset( $_POST['subject']))
-$subject = $_POST['subject'];
-
-$content="From: $name \n Email: $email \n Message: $message";
-$recipient = "g.latrofa3@studenti.poliba.it";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $content, $mailheader) or die(mysqli_error($connection));
-echo "Email sent!";
 */
+/*
+$content="From: $name \n Email: $email \n Message: $message";
+$destinatario = "g.latrofa3@studenti.poliba.it";
+$mailheader = "From: $email \r\n";
+*/
+mail($destinatario, $oggetto, $messaggio) or die(mysqli_error($connection));
+echo true;
 
 ?>
