7960ae44d4da19d8bf4fb6d995b55b47869b3458
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-06-07 12:14:39 +0200

    fix bottone modal

--- src/js/contattaci.js
+++ src/js/contattaci.js
@@ -1,5 +1,5 @@
 // dichiarazione stringhe modal
-const modalContattaciSuccess = "<div class='modal-dialog modal-dialog-centered modal-sm' role='document'><div class='modal-content'><div class='modal-header'><h5 class='modal-title'>Emial inviata</h5><button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div><div class='modal-body'><p>Grazie per averci contatto.</p></div><div class='modal-footer'><button type='button' class='btn btn-primary' id='redirectContattaciSuccess'>Torna alla home</button><button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button></div></div>";
+const modalContattaciSuccess = "<div class='modal-dialog modal-dialog-centered modal-sm' role='document'><div class='modal-content'><div class='modal-header'><h5 class='modal-title'>Email inviata</h5><button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div><div class='modal-body'><p>Grazie per averci contatto.</p></div><div class='modal-footer'><button type='button' class='btn btn-primary' id='redirectContattaciSuccess'>Torna alla home</button><button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button></div></div>";
 const modalContattaciError = "<div class='modal-dialog modal-dialog-centered modal-sm' role='document'><div class='modal-content'><div class='modal-header'><h5 class='modal-title'>Si &egrave; verificato un errore</h5><button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div><div class='modal-body'><p>Ci scusiamo per il disagio.</p></div><div class='modal-footer'><button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button></div></div>";
 
 // funzione per la memorizzazione del messaggio
@@ -16,7 +16,10 @@ $(function contattaci() {
             console.log('SUCCESS \n'+ JSON.stringify(data,null,2));
             if (data) {
                 document.getElementById('modal-contattaci-success').innerHTML = modalContattaciSuccess;
-                $('#modal-contattaci-success').modal('show');             
+                $('#modal-contattaci-success').modal('show');
+                ("#redirectContattaciSuccess").click(function (){
+                  location.href='../home.html';
+                });
             }
             else {
                 document.getElementById('modal-contattaci-error').innerHTML = modalContattaciError;
@@ -30,8 +33,4 @@ $(function contattaci() {
         }
       });
     });
-});
-
-("#redirectContattaciSuccess").click(function (){
-  location.href='../home.html';
 });
