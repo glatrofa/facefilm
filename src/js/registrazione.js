80a02d07348ea42ff48d5669c592ae0100322cd7
Alessandro Difonzo <alexdif980@gmail.com>
2020-06-08 12:44:51 +0200

    fix modalregistrazioneSuccess

--- src/js/registrazione.js
+++ src/js/registrazione.js
@@ -41,16 +41,16 @@ function redirectRegistrazioneSuccess() {
 // dichiarazione stringhe modal
 let modalRegistrazioneSuccess = "<div class='modal-dialog modal-dialog-centered modal-sm' role='document'>" +
                                   "<div class='modal-content'>" + 
-                                    "<div class='modal-header'>" + 
+                                    "<div class='modal-header bg-awwa-secondary text-white'>" + 
                                       "<h5 class='modal-title'>Registrazione effettuata</h5>" + 
-                                      "<button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button>" + 
+                                      "<button type='button' class='close text-white' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button>" + 
                                     "</div>" + 
                                     "<div class='modal-body'>" + 
                                       "<p>Grazie per esserti registrato.<br>Puoi procedere al login.</p>" + 
                                     "</div>" + 
                                     "<div class='modal-footer'>" + 
-                                      "<button type='button' class='btn btn-primary' onclick='redirectRegistrazioneSuccess()'>Vai a login</button>" + 
-                                      "<button type='button' class='btn btn-secondary' data-dismiss='modal'>Chiudi</button>" + 
+                                      "<button type='button' class='btn btn-form-primary' onclick='redirectRegistrazioneSuccess()'>Vai a login</button>" + 
+                                      "<button type='button' class='btn btn-form-secondary' data-dismiss='modal'>Chiudi</button>" + 
                                     "</div>" + 
                                   "</div>";
 
