fead329386f528598c0e356e01275f75a633b839
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-06-09 12:14:52 +0200

    aggiunto controllo su utente registrato

--- src/php/pubblica_commento.php
+++ src/php/pubblica_commento.php
@@ -10,18 +10,21 @@ $idPost = mysqli_real_escape_string($connection, $_POST["idPost"]);
 $email = mysqli_real_escape_string($connection, $_SESSION["email"]);
 $testo = mysqli_real_escape_string($connection, $_POST["modal_commento_testo"]);
 
+if (isset($_SESSION["email"])) {
+    // lock tabelle
+    $queryL = "LOCK TABLES commenti WRITE";
+    mysqli_query($connection, $queryL) or die($response = mysqli_error($connection));
+    // esecuzione query
+    $query = "INSERT INTO commenti (id_utente, id_post, testo)"
+        ."VALUES ('$email', '$idPost', '$testo')";
+    mysqli_query($connection, $query) or die($response = mysqli_error($connection));
+    // unlock tabelle
+    $queryU = "UNLOCK TABLES";
+    mysqli_query($connection, $queryU) or die ($response = mysqli_error($connection));
+    $response = true;
+}
+else
 $response = true;
-
-// lock tabelle
-$queryL = "LOCK TABLES commenti WRITE";
-mysqli_query($connection, $queryL) or die($response = mysqli_error($connection));
-// esecuzione query
-$query = "INSERT INTO commenti (id_utente, id_post, testo)"
-    ."VALUES ('$email', '$idPost', '$testo')";
-mysqli_query($connection, $query) or die($response = mysqli_error($connection));
-// unlock tabelle
-$queryU = "UNLOCK TABLES";
-mysqli_query($connection, $queryU) or die ($response = mysqli_error($connection));
 
 // ritorno dati al client
 $json_data = json_encode($response);
--- src/php/pubblica_post.php
+++ src/php/pubblica_post.php
@@ -12,20 +12,24 @@ $numeroEpisodio = mysqli_real_escape_string($connection, $_POST["numeroEpisodio"
 $testo = mysqli_real_escape_string($connection, $_POST["testo"]);
 $titolo = mysqli_real_escape_string($connection, $_POST["titolo"]);
 $email = $_SESSION["email"];
-$response = true;
 
-// lock tabelle
-$queryL = "LOCK TABLES post WRITE";
-mysqli_query($connection, $queryL) or die($response = mysqli_error($connection));
-// esecuzione query
-$query = "INSERT INTO post (email, titolo, testo, id_serie, numero_stagione, numero_episodio)"
-    ."VALUES ('$email', '$titolo', '$testo', '$idSerie', '$numeroStagione', '$numeroEpisodio')";
-$result = mysqli_query($connection, $query) or die($response = mysqli_error($connection));
-$rowsNumber = mysqli_num_rows($result);
-$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-// unlock tabelle
-$queryU = "UNLOCK TABLES";
-mysqli_query($connection, $queryU) or die ($response = mysqli_error($connection));
+if (isset($_SESSION["email"])) {
+    // lock tabelle
+    $queryL = "LOCK TABLES post WRITE";
+    mysqli_query($connection, $queryL) or die($response = mysqli_error($connection));
+    // esecuzione query
+    $query = "INSERT INTO post (email, titolo, testo, id_serie, numero_stagione, numero_episodio)"
+        ."VALUES ('$email', '$titolo', '$testo', '$idSerie', '$numeroStagione', '$numeroEpisodio')";
+    $result = mysqli_query($connection, $query) or die($response = mysqli_error($connection));
+    $rowsNumber = mysqli_num_rows($result);
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    // unlock tabelle
+    $queryU = "UNLOCK TABLES";
+    mysqli_query($connection, $queryU) or die ($response = mysqli_error($connection));
+    $response = true;
+}
+else
+    $response = false;
 
 // ritorno dati al client
 $jsonData = json_encode($response);
